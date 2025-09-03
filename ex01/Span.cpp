40229440cb6a6d81095ea7fc940178228a7799ac
Sacha Kock <skock@c3r1p4.42nice.fr>
2025-09-03 11:45:14 +0200

    ended

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -1,16 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Span.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: skock <skock@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/03 13:51:25 by skock             #+#    #+#             */
-/*   Updated: 2025/07/09 13:09:36 by skock            ###   ########.fr       */
+/*   Created: 2025/09/03 11:31:12 by skock             #+#    #+#             */
+/*   Updated: 2025/09/03 11:31:40 by skock            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(void)
+#include "Span.hpp"
+
+Span::Span()
 {
-	
+}
+
+Span::~Span()
+{
+}
+
+Span::Span(const Span &copy)
+{
+	*this = copy;
+}
+
+Span& Span::operator=(const Span &other)
+{
+	if (this != &other)
+	{
+	}
+	return (*this);
 }
